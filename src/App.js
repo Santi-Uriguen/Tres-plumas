9e7c3a65448d734801246fe7304775cbc276965a
SantiUriguen <santiuriguen@hotmail.com>
2021-12-12 15:24:04 -0300

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { useEffect } from "react";
 import "./App.css";
 import Header from "./components/header/Header";
 import Main from "./components/main/Main";
 
 function App() {
   let lastScrollTop = 0;
+  let viewport = window.innerHeight;
   window.addEventListener(
     "scroll",
     function () {
@@ -12,12 +12,12 @@ function App() {
       let st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
       if (st > lastScrollTop) {
         let scrolled = window.scrollY;
-        if ((scrolled >= 0) & (scrolled <= 700)) {
-          window.scrollTo(0, 700);
+        if ((scrolled >= 0) & (scrolled <= viewport)) {
+          window.scrollTo(0, viewport);
         }
       } else {
         let scrolled = window.scrollY;
-        if ((scrolled <= 700) & (scrolled >= 0)) {
+        if ((scrolled <= viewport) & (scrolled >= 0)) {
           window.scrollTo(0, 0);
         }
       }
